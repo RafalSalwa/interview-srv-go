3c53c8fd2d5cc91eb3e90f88a6daa102fb10c7e0
Rafal Salwa <rafal@salwa.com.pl>
2023-04-24 20:55:54 +0200

    moved logger to separate package

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,14 +2,12 @@ package main
 
 import (
 	"fmt"
-	"github.com/sirupsen/logrus"
 	"os"
 
 	unoHttp "github.com/RafalSalwa/interview/http"
 	"github.com/RafalSalwa/interview/service"
 	"github.com/RafalSalwa/interview/sql"
 	"github.com/RafalSalwa/interview/util/logger"
-	nested "github.com/antonfisher/nested-logrus-formatter"
 	"github.com/gorilla/mux"
 )
 
@@ -19,17 +17,6 @@ type App struct {
 }
 
 func main() {
-	log := logrus.New()
-	log.SetFormatter(&nested.Formatter{
-		HideKeys:    true,
-		FieldsOrder: []string{"component", "category"},
-	})
-
-	log.Info("just info message")
-	// Output: Jan _2 15:04:05.000 [INFO] just info message
-
-	log.WithField("component", "rest").Warn("warn message")
-	// Output: Jan _2 15:04:05.000 [WARN] [rest] warn message
 	env := os.Getenv("APP_ENV")
 
 	usersDb := sql.NewUsersDB()
