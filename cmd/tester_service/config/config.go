ea0a59575868bfac72297bc4a8d3b4f108bb21f1
Rafal Salwa <rafal@salwa.com.pl>
2023-06-27 00:47:19 +0200

    new service for automated tests of user registration and activation flow based on workers pool

---
+++ cmd/tester_service/config/config.go
@@ -0,0 +1,68 @@
+package config
+
+import (
+	"flag"
+	"fmt"
+	"github.com/RafalSalwa/interview-app-srv/pkg/auth"
+	"github.com/RafalSalwa/interview-app-srv/pkg/logger"
+	"github.com/pkg/errors"
+	"github.com/spf13/viper"
+	"os"
+)
+
+var configPath string
+
+func init() {
+	flag.StringVar(&configPath, "config", "", "API Gateway microservice config path")
+}
+
+type Config struct {
+	ServiceName string         `mapstructure:"serviceName"`
+	App         App            `mapstructure:"app"`
+	Logger      *logger.Config `mapstructure:"logger"`
+	Http        Http           `mapstructure:"http"`
+	Auth        auth.Auth      `mapstructure:"auth"`
+}
+
+type App struct {
+	Env   string `mapstructure:"env"`
+	Debug bool   `mapstructure:"debug"`
+}
+
+type Http struct {
+	Addr                string `mapstructure:"addr"`
+	Development         bool   `mapstructure:"development"`
+	BasePath            string `mapstructure:"basePath"`
+	DebugHeaders        bool   `mapstructure:"debugHeaders"`
+	HttpClientDebug     bool   `mapstructure:"httpClientDebug"`
+	DebugErrorsResponse bool   `mapstructure:"debugErrorsResponse"`
+}
+
+func InitConfig() (*Config, error) {
+	cfg := &Config{}
+	path, err := getEnvPath()
+	if err != nil {
+		return nil, err
+	}
+	viper.SetConfigType("yaml")
+	viper.SetConfigFile(path)
+
+	if err := viper.ReadInConfig(); err != nil {
+		return nil, errors.Wrap(err, "viper.ReadInConfig")
+	}
+
+	if err := viper.Unmarshal(cfg); err != nil {
+		return nil, errors.Wrap(err, "viper.Unmarshal")
+	}
+	return cfg, nil
+}
+
+func getEnvPath() (string, error) {
+	getwd, err := os.Getwd()
+	if err != nil {
+		return "", errors.Wrap(err, "os.Getwd")
+	}
+	configPath = fmt.Sprintf("%s/cmd/tester_service/config/config.yaml", getwd)
+
+	return configPath, nil
+}
