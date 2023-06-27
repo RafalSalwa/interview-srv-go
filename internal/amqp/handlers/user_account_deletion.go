5b7f87d35119356f1994b431c8a127fd7a26d577
Rafal Salwa <rafal@salwa.com.pl>
2023-06-27 22:11:47 +0200

    Jenkinsfile

--- internal/amqp/handlers/user_account_deletion.go
+++ internal/amqp/handlers/user_account_deletion.go
@@ -3,15 +3,14 @@ package handlers
 import (
 	"encoding/json"
 	"fmt"
-
-	"github.com/RafalSalwa/interview-app-srv/pkg/amqp"
+	"github.com/RafalSalwa/interview-app-srv/pkg/rabbitmq"
 )
 
 type CustomerAccountDeletion struct {
 	UserId int `json:"user_id"`
 }
 
-func WrapHandleUserAccountDeletion(event amqp.Event) error {
+func WrapHandleUserAccountDeletion(event rabbitmq.Event) error {
 	var data CustomerAccountDeletion
 	_ = json.Unmarshal([]byte(event.Content), &data)
 
