bf9d30875ddbaafbdd9e77cafe262beac6d57da3
Rafal Salwa <rafal@salwa.com.pl>
2023-08-29 14:27:40 +0200

    added staging config for auth service

---
+++ cmd/auth_service/config/config_test.go
@@ -0,0 +1,28 @@
+package config
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestInitConfig(t *testing.T) {
+	path, err := getEnvPath()
+	assert.NoError(t, err)
+	assert.NotEmpty(t, path)
+	assert.Contains(t, path, "auth_service")
+
+	c, err := InitConfig()
+	assert.NoError(t, err)
+	assert.NotEmpty(t, c.App)
+	assert.NotEmpty(t, c.Logger)
+	assert.NotEmpty(t, c.Jaeger)
+	assert.NotEmpty(t, c.ServiceName)
+	assert.NotEmpty(t, c.Email)
+	assert.NotEmpty(t, c.JWTToken)
+	assert.NotEmpty(t, c.Mongo)
+	assert.NotEmpty(t, c.Rabbit)
+	assert.NotEmpty(t, c.Redis)
+	assert.NotEmpty(t, c.GRPC)
+	assert.NotEmpty(t, c.MySQL)
+	assert.Equal(t, "auth_service", c.ServiceName)
+}
